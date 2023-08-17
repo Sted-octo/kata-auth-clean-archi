a0c9ebb863fe0fd020228c9e8856907a0b94afb3
ngutierrez-octo <nicolas.a.gutierrez@octo.com>
2023-08-17 15:57:41 +0200

    CHATGPT VERSION : correction du probleme de dépendances circulaires

--- Python/CHATGPT/app/interfaces/controllers.py
+++ Python/CHATGPT/app/interfaces/controllers.py
@@ -1,25 +1,30 @@
 from flask import Blueprint, request, jsonify
-from app.dependencies import auth_controller
+from app.core.use_cases import AuthenticateUseCase
+from app.dependencies import dependency_container
 
 auth_bp = Blueprint('auth', __name__)
 
 class AuthController:
-    def __init__(self, authenticate_use_case, token_service):
-        self.authenticate_use_case = authenticate_use_case
+    def __init__(self, auth_use_case, token_service):
+        self.auth_use_case = auth_use_case
         self.token_service = token_service
 
     def authenticate(self, request_data):
         name = request_data.get('name')
         password = request_data.get('password')
 
-        if self.authenticate_use_case.authenticate(name, password):
+        if self.auth_use_case.authenticate(name, password):
             token = self.token_service.generate_token()
             return jsonify(token=token), 200
         else:
             return jsonify(message="Unauthorized"), 401
-        
+
+auth_controller = AuthController(
+    AuthenticateUseCase(dependency_container.database_service),
+    dependency_container.token_service
+)
+
 @auth_bp.route('/api/auth', methods=['POST'])
 def authenticate_endpoint():
     data = request.json
     return auth_controller.authenticate(data)
-
--- Python/CHATGPT/run.py
+++ Python/CHATGPT/run.py
@@ -1,6 +1,5 @@
 from flask import Flask
 from app.interfaces.controllers import auth_bp
-from app.dependencies import auth_controller
 
 app = Flask(__name__)
 app.register_blueprint(auth_bp)
